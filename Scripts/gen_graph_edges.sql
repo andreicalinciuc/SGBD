2f7fe6dfbd75b918f542389aa4bc21dd2f515f3d
Radu Florin MARINOIU <rmarinoiu@bitdefender.com>
2019-05-07 22:22:09 +0300

    Added .idea to gitignore and added new scripts

---
+++ Scripts/gen_graph_edges.sql
@@ -0,0 +1,37 @@
+create or replace procedure generate_nodes_and_edges(
+    nr_nodes integer,
+    nr_edges integer
+) as
+    v_node1 integer;
+    v_node2 integer;
+    v_cost integer;
+    v_counter integer;
+begin
+    v_counter := 1;
+    while v_counter <= nr_edges/2 loop
+        begin
+            v_cost := dbms_random.value(1, 10);
+            v_node1 := dbms_random.value(1, nr_nodes);
+            v_node2 := dbms_random.value(1, nr_nodes);
+            while v_node1 = v_node2 loop
+                v_node2 := dbms_random.value(1, nr_nodes);
+            end loop;
+            insert into costuri_muchii (id_nod_from, id_nod_to, cost_muchie) values (v_node1, v_node2, v_cost);
+            insert into costuri_muchii (id_nod_to, id_nod_from, cost_muchie) values (v_node1, v_node2, v_cost);
+        exception when others then
+                dbms_output.put_line('fail');
+                v_counter := v_counter - 1;
+        end;
+        v_counter := v_counter + 1;
+    end loop;
+end;
+/
+set serveroutput on;
+declare
+    v_nodes integer := 100000;
+    v_edges integer := 1000000;
+begin
+    generate_nodes_and_edges(v_nodes, v_edges);
+end;
+/
+select count(*) from costuri_muchii;
