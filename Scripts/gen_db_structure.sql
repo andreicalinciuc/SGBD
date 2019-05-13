8f3ea6284d3d6c54c181a1f728bfb61476deb89b
Radu Florin MARINOIU <rmarinoiu@bitdefender.com>
2019-05-13 08:50:49 +0300

    Added sample data to db and tested get_active_routes to be working

--- Scripts/gen_db_structure.sql
+++ Scripts/gen_db_structure.sql
@@ -9,7 +9,7 @@ DROP TABLE trasee_statii CASCADE CONSTRAINTS;
 DROP TABLE costuri_muchii CASCADE CONSTRAINTS;
 /
 
-create or replace type int_list is table of integer;
+CREATE OR REPLACE type int_list is table of integer;
 /
 
 create TABLE soferi(
@@ -55,7 +55,6 @@ CREATE TABLE statii(
 
 CREATE TABLE trasee(
   id INT NOT NULL PRIMARY KEY,
-  durata number(2,2) NOT NULL,
   nume VARCHAR2(15) NOT NULL,
   created_at DATE,
   updated_at DATE
@@ -63,13 +62,13 @@ CREATE TABLE trasee(
 /
 
 CREATE TABLE trasee_statii(
-  id_traseu INT NOT NULL PRIMARY KEY,
-  id_statie_from INT,
-  id_statie_to INT,
+  id_traseu INT NOT NULL,
+  id_statie_from INT NOT NULL,
+  id_statie_to INT NOT NULL,
   FOREIGN KEY (id_traseu) REFERENCES trasee(id),
   FOREIGN KEY (id_statie_from) REFERENCES statii(id),
   FOREIGN KEY (id_statie_to) REFERENCES statii(id),
-  CONSTRAINT no_duplicate_muchii_statii UNIQUE (id_statie_from, id_statie_to),
+  primary key (id_traseu, id_statie_from, id_statie_to),
   created_at DATE,
   updated_at DATE 
 );
@@ -110,3 +109,31 @@ CREATE TABLE costuri_muchii(
     primary key (id_nod_from, id_nod_to)
 );
 /
+
+insert into statii (id, nume, adresa, created_at, updated_at) values (1, 'test1', 'test', sysdate, sysdate);
+insert into statii (id, nume, adresa, created_at, updated_at) values (2, 'test2', 'test', sysdate, sysdate);
+insert into statii (id, nume, adresa, created_at, updated_at) values (3, 'test3', 'test', sysdate, sysdate);
+insert into statii (id, nume, adresa, created_at, updated_at) values (4, 'test4', 'test', sysdate, sysdate);
+insert into statii (id, nume, adresa, created_at, updated_at) values (5, 'test5', 'test', sysdate, sysdate);
+
+insert into trasee (id, nume, created_at, updated_at) values (1, 'test1', sysdate, sysdate);
+insert into trasee (id, nume, created_at, updated_at) values (2, 'test2', sysdate, sysdate);
+insert into trasee (id, nume, created_at, updated_at) values (3, 'test3', sysdate, sysdate);
+
+insert into trasee_statii values (1, 1, 2, sysdate, sysdate);
+insert into trasee_statii values (1, 2, 3, sysdate, sysdate);
+insert into trasee_statii values (1, 3, 4, sysdate, sysdate);
+insert into trasee_statii values (1, 4, 1, sysdate, sysdate);
+insert into trasee_statii values (2, 1, 3, sysdate, sysdate);
+insert into trasee_statii values (2, 3, 4, sysdate, sysdate);
+insert into trasee_statii values (2, 4, 2, sysdate, sysdate);
+insert into trasee_statii values (2, 2, 1, sysdate, sysdate);
+insert into trasee_statii values (3, 1, 5, sysdate, sysdate);
+insert into trasee_statii values (3, 5, 1, sysdate, sysdate);
+
+insert into soferi values (1, 'test', 'ptest', 0552, 19852123, sysdate, sysdate);
+insert into depou values (1, 'test', 60, sysdate, sysdate);
+insert into vehicule_depou values (1, 30, 1, 1, sysdate, sysdate);
+insert into curse values (1, 1, 1, sysdate, sysdate);
+insert into curse values (2, 1, 1, sysdate, sysdate);
+insert into curse values (3, 2, 1, sysdate, sysdate);
