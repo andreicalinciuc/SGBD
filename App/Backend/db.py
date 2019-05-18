259faa4b34cb14a39cc6c56ee92931afd2e42e0e
Radu-Florin Marinoiu <radumarinoiu98@gmail.com>
2019-05-18 13:33:29 +0300

    Fixed db structure and changed file names

--- App/Backend/db.py
+++ App/Backend/db.py
@@ -1,25 +1,29 @@
 import cx_Oracle
 
+_conn = cx_Oracle.connect('STUDENT/STUDENT@localhost/xe')
+_cursor = _conn.cursor()
+
+def _atexit():
+    _conn.close()
+    
+
 # sql = "select * from curse"
 # print list(cursor.execute(sql))
 # print cursor.callfunc("get_traseu_cursa", cursor.arrayvar(cx_Oracle.NUMBER, []), 3)
 
 class DB:
-    _conn = None
-    _cursor = None
     def __init__(self):
-        self._conn = cx_Oracle.connect('STUDENT/STUDENT@localhost/xe')
-        self._cursor = self._conn.cursor()
-
-    def __del__(self):
-        self._conn.close()
+        pass
 
     def get_active_routes(self):
-        return self._cursor.callfunc(
-            "get_active_routes", self._cursor.var(cx_Oracle.OBJECT, typename='int_list')
+        return _cursor.callfunc(
+            "get_active_routes", _cursor.var(cx_Oracle.OBJECT, typename='int_list')
             ).aslist()
 
     def get_traseu_cursa(self, cursa_id):
-        return self._cursor.callfunc(
-            "get_traseu_cursa", self._cursor.var(cx_Oracle.OBJECT, typename='int_list'), [cursa_id]
+        return _cursor.callfunc(
+            "get_traseu_cursa", _cursor.var(cx_Oracle.OBJECT, typename='int_list'), [cursa_id]
             ).aslist()
+
+import atexit
+atexit.register(_atexit)
--- Scripts/db_structure_generator.sql
+++ Scripts/db_structure_generator.sql
@@ -9,15 +9,15 @@ DROP TABLE trasee_statii CASCADE CONSTRAINTS;
 DROP TABLE costuri_muchii CASCADE CONSTRAINTS;
 /
 
-CREATE OR REPLACE type int_list is varray(1000) of integer;
+create or replace type int_list is varray(1000) of integer;
 /
 
 create TABLE soferi(
   id INT NOT NULL PRIMARY KEY,
-  nume VARCHAR2(15) NOT NULL,
-  prenume VARCHAR2(30) NOT NULL,
+  nume VARCHAR2(50) NOT NULL,
+  prenume VARCHAR2(50) NOT NULL,
   nr_telefon	NUMBER(10,0) NOT NULL,
-  cnp	NUMBER(10,0) NOT NULL,
+  cnp	NUMBER(13,0) unique NOT NULL,
   created_at DATE,
   updated_at DATE
 );
@@ -25,8 +25,8 @@ create TABLE soferi(
 
 CREATE TABLE depou(
   id INT NOT NULL PRIMARY KEY,
-  adresa varchar2(50) NOT NULL,
-  capacitate NUMBER(3,0) NOT NULL,
+  adresa varchar2(50) unique NOT NULL,
+  capacitate int NOT NULL,
   created_at DATE,
   updated_at DATE
 );
@@ -34,20 +34,19 @@ CREATE TABLE depou(
 
 CREATE TABLE vehicule_depou(
   id INT NOT NULL PRIMARY KEY,
-  capacitate_pasageri NUMBER(3,0) NOT NULL,
+  capacitate_pasageri int NOT NULL,
   stare number(1) NOT NULL,
   id_depou int,
-  CONSTRAINT fk_depou_veh FOREIGN KEY (id_depou)
-    REFERENCES depou (id),
+  foreign key (id_depou) references depou(id),
   created_at DATE,
-  updated_at DATE       
+  updated_at DATE
 );
 /
 
 CREATE TABLE statii(
   id INT NOT NULL PRIMARY KEY,
-  nume VARCHAR2(15) NOT NULL,
-  adresa varchar2(50) NOT NULL,
+  nume VARCHAR2(15) unique NOT NULL,
+  adresa varchar2(50) unique NOT NULL,
   created_at DATE,
   updated_at DATE
 );
@@ -55,7 +54,7 @@ CREATE TABLE statii(
 
 CREATE TABLE trasee(
   id INT NOT NULL PRIMARY KEY,
-  nume VARCHAR2(15) NOT NULL,
+  nume VARCHAR2(15) unique NOT NULL,
   created_at DATE,
   updated_at DATE
 );
@@ -66,38 +65,40 @@ CREATE TABLE trasee_statii(
   id_traseu INT NOT NULL,
   id_statie_from INT NOT NULL,
   id_statie_to INT NOT NULL,
-  FOREIGN KEY (id_traseu) REFERENCES trasee(id),
-  FOREIGN KEY (id_statie_from) REFERENCES statii(id),
-  FOREIGN KEY (id_statie_to) REFERENCES statii(id),
+  foreign key (id_traseu) REFERENCES trasee(id),
+  foreign key (id_statie_from) REFERENCES statii(id),
+  foreign key (id_statie_to) REFERENCES statii(id),
   constraint trasee_uniqueness unique (id_traseu, id_statie_from, id_statie_to),
   created_at DATE,
-  updated_at DATE 
+  updated_at DATE
 );
 /
 
 CREATE TABLE curse(
-  id INT NOT NULL PRIMARY KEY,
-  id_traseu int,
-  id_soferi int,
-  FOREIGN KEY (id_traseu) REFERENCES trasee(id),
-  FOREIGN KEY (id_soferi) REFERENCES soferi(id),
+  id int not null primary key,
+  id_traseu int not null,
+  id_sofer int unique not null,
+  id_vehicul int unique not null,
+  foreign key (id_traseu) references trasee(id),
+  foreign key (id_sofer) references soferi(id),
+  foreign key (id_vehicul) references vehicule_depou(id),
   created_at DATE,
-  updated_at DATE 
+  updated_at DATE
 );
 /
 
 create table clienti(
-  nume VARCHAR2(15) NOT NULL,
-  prenume VARCHAR2(30) NOT NULL,
+  id INT NOT NULL PRIMARY KEY,
+  nume VARCHAR2(50) NOT NULL,
+  prenume VARCHAR2(50) NOT NULL,
   fonduri int,
-  id_clienti INT NOT NULL PRIMARY KEY,
   nr_telefon	NUMBER(10,0) NOT NULL,
-  email VARCHAR2(40),
-  cnp NUMBER(10,0) NOT NULL,
+  email VARCHAR2(50),
+  cnp NUMBER(13,0) NOT NULL,
+  id_cursa int,
+  foreign key (id_cursa) references curse(id),
   created_at DATE,
-  updated_at DATE,
-  id_cursa INT,
-  FOREIGN KEY (id_cursa) REFERENCES curse (id)
+  updated_at DATE
 );
 /
 
@@ -112,11 +113,11 @@ CREATE TABLE costuri_muchii(
 );
 /
 
-insert into statii (id, nume, adresa, created_at, updated_at) values (1, 'test1', 'test', sysdate, sysdate);
-insert into statii (id, nume, adresa, created_at, updated_at) values (2, 'test2', 'test', sysdate, sysdate);
-insert into statii (id, nume, adresa, created_at, updated_at) values (3, 'test3', 'test', sysdate, sysdate);
-insert into statii (id, nume, adresa, created_at, updated_at) values (4, 'test4', 'test', sysdate, sysdate);
-insert into statii (id, nume, adresa, created_at, updated_at) values (5, 'test5', 'test', sysdate, sysdate);
+insert into statii (id, nume, adresa, created_at, updated_at) values (1, 'test1', 'test1', sysdate, sysdate);
+insert into statii (id, nume, adresa, created_at, updated_at) values (2, 'test2', 'test2', sysdate, sysdate);
+insert into statii (id, nume, adresa, created_at, updated_at) values (3, 'test3', 'test3', sysdate, sysdate);
+insert into statii (id, nume, adresa, created_at, updated_at) values (4, 'test4', 'test4', sysdate, sysdate);
+insert into statii (id, nume, adresa, created_at, updated_at) values (5, 'test5', 'test5', sysdate, sysdate);
 
 insert into trasee (id, nume, created_at, updated_at) values (1, 'test1', sysdate, sysdate);
 insert into trasee (id, nume, created_at, updated_at) values (2, 'test2', sysdate, sysdate);
@@ -158,10 +159,16 @@ insert into costuri_muchii values (18, 5, 2, 5, sysdate, sysdate);
 insert into costuri_muchii values (19, 5, 3, 5, sysdate, sysdate);
 insert into costuri_muchii values (20, 5, 4, 5, sysdate, sysdate);
 
-insert into soferi values (1, 'test', 'ptest', 0552, 19852123, sysdate, sysdate);
+insert into soferi values (1, 'test', 'ptest', 0737883905, 1980512553456, sysdate, sysdate);
+insert into soferi values (2, 'test', 'ptest', 0737873905, 1980512553457, sysdate, sysdate);
+insert into soferi values (3, 'test', 'ptest', 0737863905, 1980512553458, sysdate, sysdate);
+insert into soferi values (4, 'test', 'ptest', 0737853905, 1980512553459, sysdate, sysdate);
 insert into depou values (1, 'test', 60, sysdate, sysdate);
 insert into vehicule_depou values (1, 30, 1, 1, sysdate, sysdate);
-insert into curse values (1, 1, 1, sysdate, sysdate);
-insert into curse values (2, 3, 1, sysdate, sysdate);
-insert into curse values (3, 2, 1, sysdate, sysdate);
-insert into curse values (4, 2, 1, sysdate, sysdate);
+insert into vehicule_depou values (2, 30, 1, 1, sysdate, sysdate);
+insert into vehicule_depou values (3, 30, 1, 1, sysdate, sysdate);
+insert into vehicule_depou values (4, 30, 1, 1, sysdate, sysdate);
+insert into curse values (1, 1, 1, 1, sysdate, sysdate);
+insert into curse values (2, 3, 2, 2, sysdate, sysdate);
+insert into curse values (3, 2, 3, 3, sysdate, sysdate);
+insert into curse values (4, 2, 4, 4, sysdate, sysdate);
