f99932629833835ba322a70af25deda63f29ea61
Radu-Florin Marinoiu <radumarinoiu98@gmail.com>
2019-05-18 18:39:45 +0300

    Added db config file

--- App/Backend/functions.py
+++ App/Backend/functions.py
@@ -1,6 +1,33 @@
+import json
 import cx_Oracle
 
-_conn = cx_Oracle.connect('STUDENT/STUDENT@localhost/xe')
+#Default values
+DB_CONFIG = {
+"username": "STUDENT",
+"password": "STUDENT",
+"address": "localhost",
+"database": "orcl.local"
+}
+
+try:
+    with open("db_login_info.json", "rb") as f:
+        DB_CONFIG = json.load(f)
+except:
+    try:
+        with open("db_login_info.json", "w") as f:
+            f.write(json.dumps(DB_CONFIG, indent=4))
+        print("Failed to find db_login_info.json")
+    except:
+        print("Failed to write db_login_info.json")
+
+
+_conn = cx_Oracle.connect('{}/{}@{}/{}'.format(
+    DB_CONFIG["username"],
+    DB_CONFIG["password"],
+    DB_CONFIG["address"],
+    DB_CONFIG["database"])
+    )
+
 _cursor = _conn.cursor()
 
 def get_active_routes():
@@ -8,17 +35,19 @@ def get_active_routes():
         "get_active_routes", _cursor.var(cx_Oracle.OBJECT, typename='int_list')
         ).aslist()
 
-def get_traseu_cursa():
+def get_traseu_cursa(id_cursa):
     return _cursor.callfunc(
-        "get_traseu_cursa", _cursor.var(cx_Oracle.OBJECT, typename='int_list')
+        "get_traseu_cursa", _cursor.var(cx_Oracle.OBJECT, typename='int_list', arguments=[id_cursa])
         ).aslist()
 
-def get_fastest_ride():
+def get_fastest_ride(node_from, node_to):
     return _cursor.callfunc(
-        "get_fastest_ride", _cursor.var(cx_Oracle.OBJECT, typename='int_list')
+        "get_fastest_ride", _cursor.var(cx_Oracle.OBJECT, typename='int_list', arguments=[node_from, node_to])
         ).aslist()
+
+print get_active_routes()
 
 import atexit
-def _atexit():
-    _conn.close()
-atexit.register(_atexit)
+def _atexit(connection):
+    connection.close()
+atexit.register(_atexit, connection = _conn)
--- App/Backend/procedures.py
+++ App/Backend/procedures.py
@@ -1,6 +1,33 @@
+import json
 import cx_Oracle
 
-_conn = cx_Oracle.connect('STUDENT/STUDENT@localhost/orcl.local')
+#Default values
+DB_CONFIG = {
+"username": "STUDENT",
+"password": "STUDENT",
+"address": "localhost",
+"database": "orcl.local"
+}
+
+try:
+    with open("db_login_info.json", "rb") as f:
+        DB_CONFIG = json.load(f)
+except:
+    try:
+        with open("db_login_info.json", "w") as f:
+            f.write(json.dumps(DB_CONFIG, indent=4))
+        print("Failed to find db_login_info.json")
+    except:
+        print("Failed to write db_login_info.json")
+
+
+_conn = cx_Oracle.connect('{}/{}@{}/{}'.format(
+    DB_CONFIG["username"],
+    DB_CONFIG["password"],
+    DB_CONFIG["address"],
+    DB_CONFIG["database"])
+    )
+
 _cursor = _conn.cursor()
 
 available_procedures = [
